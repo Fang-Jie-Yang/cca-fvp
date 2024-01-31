278146561069706b59dfef8be0f37cfb9e4bbc59
r12922015 <r12922015@ntu.edu.tw>
2024-01-31 19:19:34 +0800

    move shrinkwrap building commands to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,18 @@ util/kernel.sh
 util/docker.sh
 util/shrinkwrap.sh
 
+export PATH=$PWD/build/shrinkwrap/shrinkwrap:$PATH
+export SHRINKWRAP_BUILD=$PWD/build/shrinkwrap_build
+export SHRINKWRAP_PACKAGE=$PWD/build/shrinkwrap_package
+shrinkwrap build cca-3world.yaml --overlay buildroot.yaml --btvar GUEST_ROOTFS='${artifact:BUILDROOT}'
+cd ${SHRINKWRAP_PACKAGE}/cca-3world
+#e2fsck -fp rootfs.ext2
+#resize2fs rootfs.ext2 32G
+sudo su -c "mkdir mnt"
+sudo su -c "mount $ROOTFS mnt"
+sudo su -c "mkdir mnt/cca"
+sudo su -c "cp guest-disk.img KVMTOOL_EFI.fd lkvm mnt/cca/."
+sudo su -c "umount mnt"
+sudo su -c "rm -rf mnt"
+
 sudo reboot
