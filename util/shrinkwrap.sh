278ab2b62498b5faf8899d1a274d8e19423e6218
Tomato-059 <58678360+Fang-Jie-Yang@users.noreply.github.com>
2024-02-25 09:01:50 +0800

    Update shrinkwrap.sh

--- util/shrinkwrap.sh
+++ util/shrinkwrap.sh
@@ -5,4 +5,5 @@ cd $PWD/build
 yes | sudo apt update
 yes | sudo apt install git netcat-openbsd python3 python3-pip telnet
 sudo pip3 install pyyaml termcolor tuxmake
+sudo pip3 install graphlib-backport
 git clone https://git.gitlab.arm.com/tooling/shrinkwrap.git
